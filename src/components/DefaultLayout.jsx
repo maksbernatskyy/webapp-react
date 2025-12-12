1df8de82235f059b9cbad65bec321742fdb42dbd
maksbernatskyy <maksbernatskyy@gmail.com>
2025-12-12 10:57:07 +0100

    Refactor DefaultLayout and Footer components for improved structure and styling

--- src/components/DefaultLayout.jsx
+++ src/components/DefaultLayout.jsx
@@ -5,11 +5,14 @@ import Footer from "./Footer"
 export default function DefaultLayout() {
     return (
         <>
+        <div className="d-flex flex-column min-vh-100">
           <Header />
           <main>
             <Outlet />
           </main>
           <Footer />
+        </div>
+          
         </>
     )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <>
-          <footer className="bg-black">
+          <footer className="bg-dark mt-auto">
             <h2 className="text-white">Footer</h2>
           </footer>
         </>
